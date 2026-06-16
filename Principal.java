d45e00f948fb291a161cee1b73c6123381573fdf
Axel <axel@gmail.com>
2026-06-16 10:06:33 +0200

    Cambios en el main

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 	
 		System.out.println("Hola mundo");
-		System.out.println("Estamos de exemen");
+		System.out.println("Examen de Entornos");
 				
 
 
